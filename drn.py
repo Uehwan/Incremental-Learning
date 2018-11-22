35464837d336aea5b813a5df0d11136498d1130d
Uehwan <ohnefetter@kaist.ac.kr>
2018-11-22 11:01:30 +0900

    test and debugging: bugs are fixed (group add)

--- drn.py
+++ drn.py
@@ -37,7 +37,7 @@ l2_norm = partial(np.linalg.norm, ord=2, axis=-1)
 
 
 class DRN(object):
-    def __init__(self, lr=1.0, glr=1.0, alpha=1.0, rho=0.9, v=2):
+    def __init__(self, lr=0.9, glr=1.0, alpha=1.0, rho=0.9, v=2):
 
         self.lr = lr          # learning rate
         self.glr = glr        # global learning rate
@@ -49,7 +49,7 @@ class DRN(object):
         self.wg = None        # global weight vector
         self.dim = None       # dimension of input vectors
         self.n_category = 0   # number of categories
-        self.group = []       # group container
+        self.group = {}       # group container
 
     def _init_weights(self, sample):
         assert len(sample.shape) == 1, "Wrong vector for initialization, " \
@@ -57,98 +57,88 @@ class DRN(object):
         self.w, self.wg = [np.atleast_2d(np.hstack((sample, sample)))] * 2
         self.dim = sample.shape[0]
         self.n_category += 1
-        print("0. Weight Initialization")
 
     def _update_global_weight(self, sample):
         if self._distance(sample, self.wg):
-            print("7. GLOBAL UPDATE CALLED")
             self.wg = self._update_weight(sample, self.wg, self.glr)
-            print("7-1. PRINTING SELF GROUP BEFORE")
-            print(self.group)
             self._grouping()
-            print("7-1. PRINTING SELF GROUP AFTER")
             print(self.group)
 
     def _update_weight(self, sample, weight, lr):
-        print("6. UPDATE WEIGHT CALLED")
         _, _, w1, w2 = self._split_weight(sample, weight)
         updated_weight = lr * np.hstack((w1, w2)) + (1 - lr) * weight
         return updated_weight
 
     def _split_weight(self, sample, weight):
-        print("5. SPLIT WEIGHT CALLED")
         weight = np.atleast_2d(weight)
         front, back = weight[:, :self.dim], weight[:, self.dim:]
-        print("shapes of splited vectors")
-        print(front.shape, back.shape, sample.shape)
         w1, w2 = np.minimum(sample, front), np.maximum(sample, back)
         return front, back, w1, w2
 
     def _grouping(self):
-        print("10. GROUPING CALLED")
-        to_delete = []
-        for ind, connection in enumerate(self.group):
-            smaller, larger = connection[:2]  # comparison in the index order not the size
+        to_delete_group, to_delete_index = [], []
+        for connection in self.group:
+            smaller, larger = connection  # comparison in the index order not the size
+
+            # pass if already removed in the previous steps
+            if smaller in to_delete_index or larger in to_delete_index:
+                continue
+
             resonance, w_ij_1, w_ij_2 = self._resonance_between_clusters(self.w[smaller], self.w[larger])
             if resonance:
                 # merge two clusters
+                to_delete_index.append(larger)
                 self.w[smaller] = np.hstack((w_ij_1, w_ij_2))
                 self.w = np.delete(self.w, larger, axis=0)
                 self.n_category -= 1
                 # reconnect nodes previously connected to "larger" to "smaller"
                 for check in self.group:
-                    if larger in check[:2]:
-                        check[check.index(larger)] = smaller
-                        item1, item2 = min(check[0], check[1]), max(check[0], check[1])
-                        if item1 == item2:
-                            T = 0
-                            to_delete.append([item1, item2, 0])
+                    if larger in check:
+                        item1, item2 = check
+                        reconnection = {item1, item2}
+                        _, _ = reconnection.remove(larger), reconnection.add(smaller)
+                        reconnection = sorted(reconnection)
+                        if not len(reconnection) == 2:  # self-connection
+                            to_delete_group.append((item1, item2))
                         else:
-                            print("printing items")
-                            print("SELF W", self.w, self.n_category)
-                            print(item1, item2)
+                            print("reconnection: ", reconnection)
+                            item1, item2 = reconnection
                             subtraction = np.atleast_2d(self.w[item1] - self.w[item2])
                             center_of_mass_diff = (subtraction[:, :self.dim] + subtraction[:, self.dim:]) / 2
                             T = np.exp(-self.alpha * l2_norm(center_of_mass_diff))
-                        check[0], check[1], check[2] = item1, item2, T
+                            self.group[(item1, item2)] = T
         # remove inappropriate connections
-        [self.group.remove(delete) for delete in to_delete]
+        for delete in to_delete_group:
+            del self.group[delete]
 
     def _resonance_between_clusters(self, cluster1, cluster2):
         front, back = self.wg[:, :self.dim], self.wg[:, self.dim:]
         M = np.prod(back - front)
-        print("4. RESONANCE BETWEEN CLUSTERS")
         cluster1, cluster2 = np.atleast_2d(cluster1), np.atleast_2d(cluster2)
         w_i_front, w_i_back = cluster1[:, :self.dim], cluster1[:, self.dim:]
         w_j_front, w_j_back = cluster2[:, :self.dim], cluster2[:, self.dim:]
         w_ij_front, w_ij_back = np.minimum(w_i_front, w_j_front), np.maximum(w_i_back, w_j_back)
-        # _, _, w_ij_front, w_ij_back = self._split_weight(cluster1, cluster2)
         S = np.prod(w_ij_back - w_ij_front)
         return (M - S) / M > self.rho, w_ij_front, w_ij_back
 
     def _distance(self, sample, weight):
-        print("3. DISTANCE FUNC CALLED")
         f, b, w1, w2 = self._split_weight(sample, weight)
         distance = l2_norm(w1 - f + w2 - b)
         return distance
 
     def _activation(self, sample):
-        print("8. ACTIVATION CALLED")
         distance = self._distance(sample, self.w)
         activation = np.exp(-self.alpha * distance)
         return activation
 
     def _template_matching(self, sample, v_nodes):
-        print("1. TEMPLATE MATCHING CALLED")
         front, back, _, _ = self._split_weight(sample, self.wg)
         M = np.prod(back - front)
-        print("2. TEMPLATE MATCHING SECOND WEIGHT SPLIT")
         _, _, w1, w2 = self._split_weight(sample, self.w[v_nodes[0]])
         S = np.prod(w2 - w1)
         return (M - S) / M > self.rho
 
     def _add_category(self, sample):
-        print("12. ADD CATEGORY")
         self.n_category += 1
         new_weight = np.hstack((sample, sample))
         self.w = np.vstack((self.w, new_weight))
@@ -167,13 +157,13 @@ class DRN(object):
 
         print("V_NODES 33: ", to_connect)
         for first in range(len(to_connect)):
-            for second in range(first, len(to_connect)):
+            for second in range(first + 1, len(to_connect)):
                 smaller, larger = sorted([to_connect[first], to_connect[second]])
                 # new connections get added (first condition)
                 # and synaptic strengths get updated (second condition)
-                if not smaller == larger and not (smaller, larger) in self.group or condition and v_nodes[0] in (smaller, larger):
-                    T = np.exp(-self.alpha * l2_norm(center_of_mass[first] - center_of_mass[second]))
-                    self.group.append([smaller, larger, T])
+                T = np.exp(-self.alpha * l2_norm(center_of_mass[first] - center_of_mass[second]))
+                if not T == 0 or condition and v_nodes[0] in (smaller, larger):
+                    self.group[(smaller, larger)] = T
 
     def train(self, x, epochs=1):
         """
@@ -226,6 +216,11 @@ class DRN(object):
                 if self.n_category > 1:
                     self._add_group(v_node_selection, sample, resonance)
                 i += 1
+        print("Resulting model parameters:")
+        print(self.w)
+        print(self.wg)
+        print(self.group)
+        print(self.n_category)
         return self
 
     def test(self, x, train=False):
@@ -263,10 +258,10 @@ if __name__ == '__main__':
 
     random.seed(7810)
     print("TEST of DRN")
-    # data = io.loadmat('data.mat')['points']
-    data_test = np.array([[1, 1],
-                          [1.5, 0.7],
-                          [0.8, 1.2],
+    data = io.loadmat('data.mat')['points']
+    data_test = np.array([[4, 4],
+                          [-2, -2],
+                          [-5, 5],
                           [-2, 1.3],
                           [-1.5, -0.6],
                           [3, -1],
